29d65bdea1b5517910f9ecfbb4b5f676d6ad960b
오규연 <dhrbdus@naver.com>
2026-02-13 22:45:31 +0900

    260213_second

--- algorithm/IM/P11_grading.py
+++ algorithm/IM/P11_grading.py
@@ -1,10 +1,31 @@
 import sys
 
-sys.stdin = open('input_')
+sys.stdin = open('input_p11_grading.txt')
 
 T = int(input())
 
 for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    correct_answer = list(map(int, input().split()))
+    students_answer = [list(map(int, input().split())) for _ in range(N)]
 
+    students_score = []
+    
+    # 각 학생별로 순회하며 채점 진행
+    for i in range(N):
+        score = 0
+        sequence_cnt = 0  # 연속 정답일 시 추가되는 점수 카운트
+        
+        # 문항 수만큼 순회
+        for j in range(M):
+            # 정답인 경우
+            if correct_answer[j] == students_answer[i][j]:
+                sequence_cnt += 1
+                score += sequence_cnt
+            #오답인 경우
+            else:
+                sequence_cnt = 0  # 연속 점수 초기화
+        
+        students_score.append(score)
 
-    print(f'#{tc} {}')
+    print(f'#{tc} {max(students_score) - min(students_score)}')
--- algorithm/IM/P14_portal.py
+++ algorithm/IM/P14_portal.py
@@ -1,10 +1,24 @@
 import sys
 
-sys.stdin = open('input_')
+sys.stdin = open('input_p14_portal.txt')
 
 T = int(input())
 
 for tc in range(1, T + 1):
+    N = int(input())
+    P = list(map(int, input().split()))
 
+    # 1번에서 2번 방으로 이동
+    portal_cnt = 1
 
-    print(f'#{tc} {}')
+    # 2번 방(인덱스 1)부터 N-1번 방까지 순회
+    for i in range(1, N -1):
+        # 현재 방 번호: i + 1
+        # 왼쪽 포탈의 목적지 방 번호: P[i]
+        
+        # 1) 현재 방 첫 진입 시 왼쪽 P[i]로 이동: + 1
+        # 2) P[i]에서 현재 방(i+1)까지 다시 복귀하는 횟수: (i + 1) - P[i]
+        # 3) 현재 방 재진입 시 오른쪽으로 이동: + 1
+        portal_cnt += i + 3 - P[i]
+
+    print(f'#{tc} {portal_cnt}')
