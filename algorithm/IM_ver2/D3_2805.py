249ea9d475389329f1fbdc561e90873473785b85
오규연 <dhrbdus@naver.com>
2026-02-19 03:00:42 +0900

    260218_first

--- algorithm/IM_ver2/D3_2805.py
+++ algorithm/IM_ver2/D3_2805.py
@@ -1,10 +1,13 @@
 import sys
 
-sys.stdin = open('input_')
+sys.stdin = open('input_2805.txt')
 
 T = int(input())
 
 for test_case in range(1, T + 1):
+    N = int(input())
+    farm = [list(map(int, input().split())) for _ in range(N)]
 
+    M = N // 2
 
     print(f'#{test_case} {}')
